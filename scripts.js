4a42235377742a8f2772bc1e326b88e91b1c4546
danilovalerio <danilo_vs@hotmail.com>
2023-05-04 15:40:15 -0300

    Limpar tudo, adicionar tarefa, marcar como feita

--- scripts.js
+++ scripts.js
@@ -1,8 +1,38 @@
+var tasks = [
+    { 
+        titulo: 'Tarefa Um',
+        feita: false
+    },
+    { 
+        titulo: 'Tarefa Um',
+        feita: false
+    },
+    { 
+        titulo: 'Tarefa Um',
+        feita: true
+    },
+]
 
 const todoListApp = {
     data() {
         return {
-            mensagem:  "Aplicacao Rodando"
+            tasks:  window.tasks,
+            newTask: { feita: false }
+        }
+    },
+
+    methods: {
+        clearTasks: function(){
+            this.tasks = []
+        },
+
+        addTask: function(){
+            if(this.newTask.titulo) {
+                this.tasks.push(this.newTask)
+                this.newTask = {}
+            } else {
+                alert("Não pode ter título em branco, preencha e tente novamente!")
+            }
         }
     },
 }
