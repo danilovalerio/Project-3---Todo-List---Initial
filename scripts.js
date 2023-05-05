6824332e064413fa61e447d0a6d4ea136d283c7e
danilovalerio <danilo_vs@hotmail.com>
2023-05-05 10:07:32 -0300

    atualizacao da lista de tasks e v-on keyup para adicionar com enter

--- scripts.js
+++ scripts.js
@@ -10,9 +10,25 @@ const todoListApp = {
         }
     },
 
+    created() {
+         //vamos validar se tem o item no localStorage
+         this.tasks = localStorage.getItem("tasks") ?  JSON.parse(localStorage.getItem("tasks")) : this.tasks
+    },
+
+    beforeDestroy() {
+        document.removeEventListener('keyup', this.onEsc)
+    },
+
+    //pegamos os dados depois de atualizar com updated
+    updated() {
+        //esse é chamado toda vez que muda um texto ou algo do tipo
+        //localStorage.setItem("tasks", JSON.stringify(this.tasks))
+    },
+
     methods: {
         clearTasks: function(){
             this.tasks = []
+            this.storeTodos()
         },
 
         addTask: function(){
@@ -20,15 +36,16 @@ const todoListApp = {
                 this.tasks.push(this.newTask)
                 this.newTask = { feita: false}
                 localStorage.setItem("tasks", JSON.stringify(this.tasks))
+                this.storeTodos()
             } else {
                 alert("Não pode ter título em branco, preencha e tente novamente!")
             }
+        },
+
+        storeTodos(){
+            localStorage.setItem("tasks", JSON.stringify(this.tasks))
         }
     },
-    created() {
-        //vamos validar se tem o item no localStorage
-        this.tasks = localStorage.getItem("tasks") ?  JSON.parse(localStorage.getItem("tasks")) : this.tasks
-    }
 }
 
 Vue.createApp(todoListApp).mount('#app')
